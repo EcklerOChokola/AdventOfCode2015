23b44d01576178ec1219e7beb0a8055c3c96b69e
EcklerOChokola <antoine.lucas1@insa-rennes.fr>
2022-12-04 21:47:53 +0100

    End day 3

--- Day3/script.js
+++ Day3/script.js
@@ -2,13 +2,54 @@ const fs = require('fs');
 const path = require('path');
 const { cwd } = require('process');
 
+let houses = {};
+
 function extractChars(){
     const content = fs.readFileSync(path.join(cwd(), 'input.txt'));
     let lines = content.toLocaleString().split('');
     return lines;
 }
 
+function moveSanta([x, y], char){
+    if(char == '^') y++;
+    if(char == 'v') y--;
+    if(char == '>') x++;
+    if(char == '<') x--;
+    return [x, y];
+}
+
+function addMove(tab, position){
+    if(!position in tab){
+        tab[position] = 1;
+    }else{
+        tab[position] += 1;
+    }
+}
+
+let santasPosition = [0, 0];
+addMove(houses, santasPosition);
+
 const chars = extractChars();
 
-// console.log(`Result for part 1 is ${sumSurfacesFromLines(lines)}`);
-// console.log(`Result for part 2 is ${sumWrapsFromLines(lines)}`);
+for(let i = 0; i < chars.length; i++){
+    santasPosition = moveSanta(santasPosition, chars[i]);
+    addMove(houses, santasPosition);
+}
+
+let houses2 = {}; 
+santasPosition = [0, 0];
+let robSantasPosition = santasPosition;
+addMove(houses2, santasPosition);
+
+for(let i = 0; i < chars.length; i++){
+    if(i%2 == 0){
+        santasPosition = moveSanta(santasPosition, chars[i]);
+        addMove(houses2, santasPosition);
+    }else{
+        robSantasPosition = moveSanta(robSantasPosition, chars[i]);
+        addMove(houses2, robSantasPosition);
+    }
+}
+
+console.log(`Result for part 1 is ${Object.keys(houses).length}`);
+console.log(`Result for part 2 is ${Object.keys(houses2).length}`);
